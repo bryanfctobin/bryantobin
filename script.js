5ff6c645ac05d782dcbc688db69eed4559891774
Bryan Tobin <bryanfctobin@gmail.com>
2018-05-08 10:55:07 -0400

    Updating home page

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 new Vue({
     el: '#main',
     data: {
-       image: '/content/bryan-face.png',
-       link: 'https://youtu.be/SUtziaZlDeE'
+        image: '/content/bryan-face.png',
+        /*link: 'https://youtu.be/SUtziaZlDeE',*/
+        link: 'https://youtu.be/ji5_MqicxSo',
+        textOne: 'Be Brave and Be Kind'
     },
     methods: {  
     }
