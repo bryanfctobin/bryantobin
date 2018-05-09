f40c8ffe5f69a421a9803ca931254d46493967a8
Bryan Tobin <bryanfctobin@gmail.com>
2018-05-09 09:02:19 -0400

    Added function to change background color on hover

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-new Vue({
+var vm = new Vue({
     el: '#main',
     data: {
         image: '/content/bryan-face.png',
@@ -6,6 +6,14 @@ new Vue({
         link: 'https://youtu.be/ji5_MqicxSo',
         textOne: 'Be Brave and Be Kind'
     },
-    methods: {  
+    methods: {
+        
     }
 });
+function changeBG() {
+    var bgcolors = ['#33CCCC', '#A6E4E7', '#2980B9', '#2C3E50','#30E3CA','#30E3CA','#7B99FA','#53CDD8','#74F9FF','#00E0FF'];
+    var randNum = Math.floor(Math.random() * Math.floor(bgcolors.length));
+    var docBody = document.body;
+    docBody.style.backgroundColor = bgcolors[randNum];
+}
+changeBG();
